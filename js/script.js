7d16462bb83b0c972d0511048ee2ececaef23340
Aodxfaionn <aodxfaionn@yandex.ru>
2024-03-31 23:41:06 +0300

    selector for slider

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 // Слайдер
-const tabName = ['Clients', 'Website', 'Appointments', 'Actions', 'Socials']
+const tabName = ["Clients", "Website", "Appointments", "Actions", "Socials"];
 
 const swiper = new Swiper(".swiper", {
-  effect: 'creative',
+  effect: "creative",
   creativeEffect: {
     prev: {
-      opacity: 0
+      opacity: 0,
     },
     next: {
-      opacity: 0
+      opacity: 0,
     },
   },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
-    renderBullet: (index, className) => { return `<button class="${className}"><p><span>${tabName[index]}</span></p></button>` }
+    renderBullet: (index, className) => {
+      return `<button class="${className}"><p><span>${tabName[index]}</span></p></button>`;
+    },
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
+
+const line = document.querySelector(".line");
+
+swiper.on("slideChange", function (e) {
+  line.classList = 'line';
+  line.classList.add(`line-${swiper.realIndex + 1}`);
+});
+
