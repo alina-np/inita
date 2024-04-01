a3116756c37b6059b1236391efbbf0a39689d01e
versta <versta@versta.com>
2024-04-01 18:19:37 +0300

    add animation head

--- js/script.js
+++ js/script.js
@@ -24,10 +24,47 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
-const line = document.querySelector(".line");
+const line = document.querySelector(".line"),
+  hand = document.querySelector(".hand");
 
 swiper.on("slideChange", function (e) {
-  line.classList = 'line';
+  line.classList = "line";
   line.classList.add(`line-${swiper.realIndex + 1}`);
+  hand.classList = "hand";
+  hand.classList.add(`hand-${swiper.realIndex + 1}`);
 });
 
+
+// swiper.on("slideChange", function (e) {
+// gsap.from(".clients .slideHead", {
+//   opacity: 0,
+//   y: -150,
+//   duration: 0.3,
+//   ease: "power1.inOut",
+// });
+
+// gsap.from(".website .slideHead", {
+//   y: 150,
+//   duration: 0.3,
+//   ease: "power1.inOut",
+// });
+
+// gsap.from(".appointments .slideHead", {
+//   opacity: 0,
+//   scale: 1.5,
+//   duration: 0.3,
+//   ease: "power1.inOut",
+// });
+
+// gsap.from(".actions .slideHead", {
+//   scale: 0,
+//   duration: 0.3,
+//   ease: "power1.inOut",
+// });
+
+// gsap.from(".socials .slideHead", {
+//   opacity: 0,
+//   duration: 0.3,
+//   ease: "power1.inOut",
+// });
+// })
