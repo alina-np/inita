944b41489342d49e04de3ef1e1961e94df9988e0
versta <versta@versta.com>
2024-04-23 18:34:42 +0300

    new clients

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const tabName = ["Clients", "Website", "Appointments", "Actions", "Socials"],
   line = document.querySelector(".line"),
   hand = document.querySelector(".hand"),
   cardBg = document.querySelector(".website__cards"),
-  tlAction = gsap.timeline({ repeat: -1, repeatDelay: 0.1 });
+  tlAction = gsap.timeline({ repeat: -1, repeatDelay: 0.2 }),
+  tlSocials = gsap.timeline({ repeat: -1, repeatDelay: 2 });
 
 const swiper = new Swiper(".swiperMain", {
   effect: "creative",
@@ -29,7 +30,7 @@ const swiper = new Swiper(".swiperMain", {
   },
   allowTouchMove: false,
   on: {
-    init: startAnimationClients(),
+    // init: startAnimationClients(),
   },
 });
 
@@ -37,6 +38,7 @@ swiper.on("slideChange", function (e) {
   // if (swiper.realIndex === 1) starWebsiteSlider();
   // if (swiper.realIndex === 2) startAnimationAppointments();
   swiper.realIndex === 3 ? tlAction.play() : tlAction.pause();
+  swiper.realIndex === 4 ? tlSocials.play() : tlSocials.pause();
   changeStyleSwiper(swiper.realIndex);
 });
 
@@ -76,213 +78,7 @@ function changeStyleSwiper(num) {
 }
 
 // Анимация в слайдере
-function startAnimationAppointments() {
-  const tlAppointments = gsap.timeline({ repeat: 0 });
-  tlAppointments
-    .fromTo(
-      ".appointments__icons div",
-      {
-        y: 30,
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        y: 0,
-        stagger: 0.2,
-      }
-    )
-    .fromTo(
-      ".appointments__cliensList",
-      {
-        opacity: 0,
-        y: -30,
-      },
-      {
-        opacity: 1,
-        y: 0,
-      }
-    )
-    .fromTo(
-      ".appointments__infografika-border",
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: -0.2,
-      }
-    )
-    .fromTo(
-      ".appointments__infografika svg",
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: -0.2,
-      }
-    )
-    .fromTo(
-      ".appointments__infografika-big",
-      {
-        opacity: 0,
-        x: 30,
-      },
-      {
-        opacity: 1,
-        x: 0,
-        delay: -0.2,
-      }
-    )
-    .fromTo(
-      ".appointments__infografika-small",
-      {
-        opacity: 0,
-        x: -30,
-      },
-      {
-        opacity: 1,
-        x: 0,
-        delay: -0.2,
-      }
-    )
-    .fromTo(
-      ".appointments__bookings",
-      {
-        opacity: 0,
-        y: 30,
-      },
-      {
-        opacity: 1,
-        y: 0,
-        stagger: 0.1,
-        delay: -0.2,
-      }
-    );
-}
-
-tlAction
-  .fromTo(
-    ".actioncard-1",
-    { scale: 0.85, y: -45 },
-    {
-      scale: 1,
-      y: 0,
-      duration: 0.1,
-    }
-  )
-  .to(".actioncard-1 .actioncard-bg", {
-    opacity: 1,
-    duration: 0.4,
-    delay: 0.5,
-  })
-  .to(".actioncard-1", {
-    opacity: 0,
-    transformOrigin: "center",
-    x: -1300,
-    y: 30,
-    rotate: -40,
-    duration: 0.3,
-    delay: 3,
-  })
-  .to(".actioncard-1", {
-    scale: 0.85,
-    y: -45,
-    x: 0,
-    rotate: 0,
-    zIndex: -13,
-  })
-  .fromTo(
-    ".actioncard-2",
-    { scale: 0.85, y: -45 },
-    {
-      scale: 1,
-      y: 0,
-      duration: 0.1,
-    }
-  )
-  .to(".actioncard-2 .actioncard-bg", {
-    opacity: 1,
-    duration: 0.5,
-    delay: 1,
-  })
-  .to(".actioncard-2", {
-    opacity: 0,
-    transformOrigin: "center",
-    x: 1300,
-    y: 30,
-    rotate: 40,
-    duration: 0.3,
-    delay: 3,
-  })
-  .to(".actioncard-2", {
-    scale: 0.85,
-    y: -45,
-    x: 0,
-    rotate: 0,
-    zIndex: -13,
-  })
-  .fromTo(
-    ".actioncard-3",
-    { scale: 0.85, y: -45 },
-    {
-      scale: 1,
-      y: 0,
-      duration: 0.1,
-    }
-  )
-  .to(".actioncard-3 .actioncard-bg", {
-    opacity: 1,
-    duration: 0.5,
-    delay: 1,
-  })
-  .to(".actioncard-3", {
-    opacity: 0,
-    transformOrigin: "center",
-    x: 1300,
-    y: 30,
-    rotate: 40,
-    duration: 0.3,
-    delay: 3,
-  })
-  .to(".actioncard-3", {
-    scale: 0.85,
-    y: -45,
-    x: 0,
-    rotate: 0,
-    zIndex: -13,
-  })
-  .fromTo(
-    ".actioncard-4",
-    { scale: 0.85, y: -45 },
-    {
-      scale: 1,
-      y: 0,
-      duration: 0.1,
-    }
-  )
-  .to(".actioncard-4 .actioncard-bg", {
-    opacity: 1,
-    duration: 0.5,
-    delay: 1,
-  })
-  .to(".actioncard-4", {
-    opacity: 0,
-    transformOrigin: "center",
-    x: 1300,
-    y: 30,
-    rotate: 40,
-    duration: 0.3,
-    delay: 3,
-  })
-  .to(".actioncard-4", {
-    scale: 0.85,
-    y: -45,
-    x: 0,
-    rotate: 0,
-    zIndex: -13,
-  });
-
+// Блок Clients
 function startAnimationClients() {
   const tlClients = gsap.timeline({ repeat: -1, repeatDelay: 4 });
   tlClients
@@ -394,83 +190,373 @@ function startAnimationClients() {
     );
 }
 
-function startAnimationSocials() {
-  const tlSocials = gsap.timeline({});
-  tlSocials
-    .from(".socials__about", {
-      opacity: 0,
-      stagger: 0.2,
-    })
-    .to(".socials__about-bg", {
-      x: 0,
-    })
-    .to(".socials__about-bg", {
-      x: 10,
-      opacity: 0,
-    })
-    .from(".socials__about:not(:nth-of-type(3))", {
-      opacity: 0,
-      delay: -0.1,
-      duration: 0.5,
-    })
-    .to(".socials__about:nth-of-type(3)", {
-      y: -120,
-    })
-    .to(".socials__create", {
-      opacity: 0,
-      y: -300,
-      delay: 0.3,
-    })
-    .to(".socials__about:nth-of-type(3)", {
-      opacity: 0,
-      y: -300,
-      delay: -0.5,
-    })
-    .to(".socials__icon-plus", {
-      opacity: 0,
-      delay: -0.1,
-    })
-    .to(".socials__icon-back", {
-      opacity: 1,
-      delay: -0.1,
-    })
-    .to(".socials__list", {
-      opacity: 0,
-    })
-    .to(".post > *", {
-      opacity: 0,
-      delay: -0.1,
-    })
+// Блок Appointments
+function startAnimationAppointments() {
+  const tlAppointments = gsap.timeline({ repeat: 0 });
+  tlAppointments
     .fromTo(
-      ".post",
+      ".appointments__icons div",
       {
-        y: -600,
+        y: 30,
         opacity: 0,
-        delay: -0.5,
       },
       {
-        y: -600,
         opacity: 1,
+        y: 0,
+        stagger: 0.2,
       }
     )
-    .to(".post__top", {
-      opacity: 1,
-    })
-    .to(".post__photo", {
-      opacity: 1,
-    })
-    .to(".post__text", {
-      opacity: 1,
-    })
-    .to(".post__btn-color", {
-      opacity: 1,
-    })
-    .to(".post__btn-black", {
-      opacity: 1,
-    });
+    .fromTo(
+      ".appointments__cliensList",
+      {
+        opacity: 0,
+        y: -30,
+      },
+      {
+        opacity: 1,
+        y: 0,
+      }
+    )
+    .fromTo(
+      ".appointments__infografika-border",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: -0.2,
+      }
+    )
+    .fromTo(
+      ".appointments__infografika svg",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: -0.2,
+      }
+    )
+    .fromTo(
+      ".appointments__infografika-big",
+      {
+        opacity: 0,
+        x: 30,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        delay: -0.2,
+      }
+    )
+    .fromTo(
+      ".appointments__infografika-small",
+      {
+        opacity: 0,
+        x: -30,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        delay: -0.2,
+      }
+    )
+    .fromTo(
+      ".appointments__bookings",
+      {
+        opacity: 0,
+        y: 30,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        stagger: 0.1,
+        delay: -0.2,
+      }
+    );
 }
 
-startAnimationSocials();
+// Блок Action
+tlAction.pause();
+tlAction
+  .fromTo(
+    ".actioncard-1",
+    { scale: 0.85, y: -45 },
+    {
+      scale: 1,
+      y: 0,
+      duration: 0.1,
+    }
+  )
+  .to(".actioncard-1 .actioncard-bg", {
+    opacity: 1,
+    duration: 0.4,
+    delay: 0.5,
+  })
+  .to(".actioncard-1 .activemess .actioncard__borderText__container", {
+    background: "transparent",
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-1 .activemess-bg", {
+    scaleX: 1,
+    transformOrigin: "-10px",
+    delay: -0.1,
+  })
+  .to(".actioncard-1 .activemess", {
+    border: "2px solid transparent",
+    delay: -0.1,
+  })
+  .to(".actioncard-1 .activemess .actioncard__borderText__container", {
+    color: "#fff",
+    delay: -0.7,
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-1 .activemess path", {
+    fill: "#fff",
+    delay: -0.7,
+  })
+  .to(".actioncard-1", {
+    opacity: 0,
+    transformOrigin: "center",
+    x: -1300,
+    y: 30,
+    rotate: -40,
+    duration: 0.3,
+    delay: 1.5,
+  })
+  .to(".actioncard-1", {
+    scale: 0.85,
+    y: -45,
+    x: 0,
+    rotate: 0,
+    zIndex: -13,
+  })
+  .fromTo(
+    ".actioncard-2",
+    { scale: 0.85, y: -45 },
+    {
+      scale: 1,
+      y: 0,
+      duration: 0.1,
+    }
+  )
+  .to(".actioncard-2 .actioncard-bg", {
+    opacity: 1,
+    duration: 0.5,
+    delay: 1,
+  })
+  .to(".actioncard-2 .activemess .actioncard__borderText__container", {
+    background: "transparent",
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-2 .activemess-bg", {
+    scaleX: 1,
+    transformOrigin: "-10px",
+    delay: -0.1,
+  })
+  .to(".actioncard-2 .activemess", {
+    border: "2px solid transparent",
+    delay: -0.1,
+  })
+  .to(".actioncard-2 .activemess .actioncard__borderText__container", {
+    color: "#fff",
+    delay: -0.7,
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-2 .activemess path", {
+    fill: "#fff",
+    delay: -0.7,
+  })
+  .to(".actioncard-2", {
+    opacity: 0,
+    transformOrigin: "center",
+    x: 1300,
+    y: 30,
+    rotate: 40,
+    duration: 0.3,
+    delay: 1.5,
+  })
+  .to(".actioncard-2", {
+    scale: 0.85,
+    y: -45,
+    x: 0,
+    rotate: 0,
+    zIndex: -13,
+  })
+  .fromTo(
+    ".actioncard-3",
+    { scale: 0.85, y: -45 },
+    {
+      scale: 1,
+      y: 0,
+      duration: 0.1,
+    }
+  )
+  .to(".actioncard-3 .actioncard-bg", {
+    opacity: 1,
+    duration: 0.5,
+    delay: 1,
+  })
+  .to(".actioncard-3 .activemess .actioncard__borderText__container", {
+    background: "transparent",
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-3 .activemess-bg", {
+    scaleX: 1,
+    transformOrigin: "-10px",
+    delay: -0.1,
+  })
+  .to(".actioncard-3 .activemess", {
+    border: "2px solid transparent",
+    delay: -0.1,
+  })
+  .to(".actioncard-3 .activemess .actioncard__borderText__container", {
+    color: "#fff",
+    delay: -0.7,
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-3 .activemess path", {
+    fill: "#fff",
+    delay: -0.7,
+  })
+  .to(".actioncard-3", {
+    opacity: 0,
+    transformOrigin: "center",
+    x: 1300,
+    y: 30,
+    rotate: 40,
+    duration: 0.3,
+    delay: 1.5,
+  })
+  .to(".actioncard-3", {
+    scale: 0.85,
+    y: -45,
+    x: 0,
+    rotate: 0,
+    zIndex: -13,
+  })
+  .fromTo(
+    ".actioncard-4",
+    { scale: 0.85, y: -45 },
+    {
+      scale: 1,
+      y: 0,
+      duration: 0.1,
+    }
+  )
+  .to(".actioncard-4 .actioncard-bg", {
+    opacity: 1,
+    duration: 0.5,
+    delay: 1,
+  })
+  .to(".actioncard-4 .activemess .actioncard__borderText__container", {
+    background: "transparent",
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-4 .activemess-bg", {
+    scaleX: 1,
+    transformOrigin: "-10px",
+    delay: -0.1,
+  })
+  .to(".actioncard-4 .activemess", {
+    border: "2px solid transparent",
+    delay: -0.1,
+  })
+  .to(".actioncard-4 .activemess .actioncard__borderText__container", {
+    color: "#fff",
+    delay: -0.7,
+    transformOrigin: "-10px",
+  })
+  .to(".actioncard-4 .activemess path", {
+    fill: "#fff",
+    delay: -0.7,
+  })
+  .to(".actioncard-4", {
+    opacity: 0,
+    transformOrigin: "center",
+    x: 1300,
+    y: 30,
+    rotate: 40,
+    duration: 0.3,
+    delay: 1.5,
+  })
+  .to(".actioncard-4", {
+    scale: 0.85,
+    y: -45,
+    x: 0,
+    rotate: 0,
+    zIndex: -13,
+  });
+  
+// Блок Socials
+tlSocials.pause();
+tlSocials
+  .from(".socials__about", {
+    opacity: 0,
+    stagger: 0.3,
+  })
+  .to(".socials__about-bg", {
+    x: 0,
+  })
+  .to(".socials__about-bg", {
+    x: 10,
+    opacity: 0,
+  })
+  .from(".socials__about:not(:nth-of-type(3))", {
+    opacity: 0,
+    delay: -0.1,
+    duration: 0.5,
+  })
+  .to(".socials__about:nth-of-type(3)", {
+    y: -120,
+  })
+  .to(".socials__create", {
+    opacity: 0,
+    y: -300,
+    delay: 0.5,
+  })
+  .to(".socials__about:nth-of-type(3)", {
+    opacity: 0,
+    y: -300,
+    delay: -0.5,
+  })
+  .to(".socials__icon-plus", {
+    opacity: 0,
+    delay: -0.1,
+  })
+  .to(".socials__icon-back", {
+    opacity: 1,
+    delay: -0.1,
+  })
+  .to(".socials__list", {
+    opacity: 0,
+  })
+  .to(".post > * ", {
+    opacity: 0,
+    delay: -0.1,
+  })
+  .fromTo(
+    ".post",
+    {
+      y: -600,
+      opacity: 0,
+      delay: -0.5,
+    },
+    {
+      y: -600,
+      opacity: 1,
+    }
+  )
+  .to(".post > * ", {
+    opacity: 1,
+    stagger: 0.7,
+    delay: -0.8,
+  })
+  .to(".post > * ", {
+    opacity: 0,
+    delay: 0.7,
+    duration: 1,
+  });
 
 // Перетаскивание кнопок
 if (innerWidth < 768) {
@@ -491,7 +577,7 @@ function changePositionBtns() {
   pagBlock.addEventListener("touchmove", function (e) {
     currentX = e.touches[0].clientX - startX;
     if (currentX > 325) currentX = 325;
-    if (currentX < -335) currentX = -325;
+    if (currentX < -325) currentX = -325;
     pagBlock.style.left = currentX + "px";
   });
   pagBlock.addEventListener("click", function (e) {
